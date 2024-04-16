79c645f1242f26c2accec0c4567575e4239df964
Victor Hong <victor.hong@onlia.ca>
2024-04-15 21:46:06 -0400

    Created project Added netflix colours

---
+++ NetflixSwiftUI/NetflixSwiftUI/NetflixSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixSwiftUIApp.swift
+//  NetflixSwiftUI
+//
+//  Created by Victor Hong on 2024-04-15.
+//
+
+import SwiftUI
+
+@main
+struct NetflixSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
