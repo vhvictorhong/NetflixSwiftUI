ae33388b87109b4f64727479c73e898b18c3ca17
Victor Hong <victor.hong@onlia.ca>
2024-04-26 22:01:08 -0400

    Added animation to header view Added gradient view

--- NetflixSwiftUI/NetflixSwiftUI/Netflix/Core/NetflixHomeView.swift
+++ NetflixSwiftUI/NetflixSwiftUI/Netflix/Core/NetflixHomeView.swift
@@ -13,6 +13,8 @@ struct NetflixHomeView: View {
     @State private var filters = FilterModel.mockArray
     @State private var selectedFilter: FilterModel? = nil
     @State private var fullHeaderSize: CGSize = .zero
+    @State private var scrollViewOffset: CGFloat = 0
+    
     @State private var heroProduct: ProductArray.Product? = nil
     @State private var currentUser: UserArray.User? = nil
     @State private var productRows: [ProductRow] = []
@@ -20,6 +22,7 @@ struct NetflixHomeView: View {
     var body: some View {
         ZStack(alignment: .top) {
             Color.netflixBlack.ignoresSafeArea()
+            gradientView
             scrollView
             filterBarView
         }
@@ -28,6 +31,40 @@ struct NetflixHomeView: View {
             await getData()
         }
         .toolbar(.hidden, for: .navigationBar)
+    }
+    
+    private var gradientView: some View {
+        ZStack {
+            LinearGradient(
+                colors: [
+                    .netflixDarkGray.opacity(
+                        1
+                    ),
+                    .netflixDarkGray.opacity(
+                        0
+                    )
+                ],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            LinearGradient(
+                colors: [
+                    .netflixDarkRed.opacity(
+                        0.5
+                    ),
+                    .netflixDarkRed.opacity(
+                        0
+                    )
+                ],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+        }
+        .frame(maxHeight: max(10, (400 + (scrollViewOffset * 0.75))))
+        .opacity(scrollViewOffset < -250 ? 0 : 1)
+        .animation(.easeInOut, value: scrollViewOffset)
     }
     
     private var header: some View {
@@ -53,22 +90,43 @@ struct NetflixHomeView: View {
         VStack(spacing: 0) {
             header
                 .padding(.horizontal, 16)
-            NetflixFilterBarView(
-                filters: filters,
-                selectedFilter: selectedFilter) { newFilter in
-                    selectedFilter = newFilter
-                } onXMarkPressed: {
-                    selectedFilter = nil
-                }.padding(.top, 16)
+            if scrollViewOffset > -20 {
+                NetflixFilterBarView(
+                    filters: filters,
+                    selectedFilter: selectedFilter) { newFilter in
+                        selectedFilter = newFilter
+                    } onXMarkPressed: {
+                        selectedFilter = nil
+                    }
+                    .padding(.top, 16)
+                    .transition(.move(edge: .top).combined(with: .opacity))
+            }
         }
-        .background(Color.blue)
+        .padding(.bottom, 8)
+        .background(
+            ZStack {
+                if scrollViewOffset < -70 {
+                    Rectangle()
+                        .fill(Color.clear)
+                        .background(.ultraThinMaterial)
+                        .brightness(-0.2)
+                        .ignoresSafeArea()
+                }
+            }
+        )
+        .animation(.smooth, value: scrollViewOffset)
         .readingFrame { frame in
-            fullHeaderSize = frame.size
+            if fullHeaderSize == .zero {
+                fullHeaderSize = frame.size
+            }
         }
     }
     
     private var scrollView: some View {
-        ScrollView(.vertical) {
+        ScrollViewWithOnScrollChanged(
+            .vertical,
+            showsIndicators: false
+        ) {
             VStack(spacing: 8) {
                 Rectangle()
                     .opacity(0)
@@ -78,8 +136,9 @@ struct NetflixHomeView: View {
                 }
                 movieCells
             }
+        } onScrollChanged: { offset in
+            scrollViewOffset = min(0, offset.y)
         }
-        .scrollIndicators(.hidden)
     }
     
     private func heroCell(product: ProductArray.Product) -> some View {
@@ -136,7 +195,7 @@ struct NetflixHomeView: View {
             var rows: [ProductRow] = []
             let allBrands = Set(products.map({ $0.brand }))
             for brand in allBrands {
-                rows.append(ProductRow(title: brand.capitalized, products: products))
+                rows.append(ProductRow(title: brand.capitalized, products: products.shuffled()))
             }
             productRows = rows
         } catch {
