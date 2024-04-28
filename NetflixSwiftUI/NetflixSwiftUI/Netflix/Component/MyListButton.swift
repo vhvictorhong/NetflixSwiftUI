8f67fcc9faeed6cd3ea048215b6a9bdc32a1d522
Victor Hong <victor.hong@onlia.ca>
2024-04-27 22:43:12 -0400

    Added MyListButton

---
+++ NetflixSwiftUI/NetflixSwiftUI/Netflix/Component/MyListButton.swift
@@ -0,0 +1,56 @@
+//
+//  MyListButton.swift
+//  NetflixSwiftUI
+//
+//  Created by Victor Hong on 2024-04-27.
+//
+
+import SwiftUI
+
+struct MyListButton: View {
+    
+    var isMyList: Bool = false
+    var onButtonPressed: (() -> Void)? = nil
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Image(systemName: "checkmark")
+                    .opacity(isMyList ? 1 : 0)
+                    .rotationEffect(Angle(degrees: isMyList ? 0 : 180))
+                Image(systemName: "plus")
+                    .opacity(isMyList ? 0 : 1)
+                    .rotationEffect(Angle(degrees: isMyList ? -180 : 0))
+            }
+            .font(.title)
+            Text("My List")
+                .font(.caption)
+                .foregroundStyle(.netflixLightGray)
+        }
+        .foregroundStyle(.netflixWhite)
+        .padding(8)
+        .background(Color.black.opacity(0.001))
+        .animation(.bouncy, value: isMyList)
+        .onTapGesture {
+            onButtonPressed?()
+        }
+    }
+}
+
+fileprivate struct MyListButtonPreview: View {
+    
+    @State private var isMyList: Bool = false
+    
+    var body: some View {
+        MyListButton(isMyList: isMyList) {
+            isMyList.toggle()
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        MyListButtonPreview()
+    }
+}
